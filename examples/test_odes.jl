f7cd3c616e4bd0941e5471e58640a20dca7f43a9
Samuel Isaacson <isaacsas@users.noreply.github.com>
2019-04-16 18:02:34 -0400

    testing jacobian usage

--- examples/test_odes.jl
+++ examples/test_odes.jl
@@ -14,13 +14,15 @@ reset_timer!(to)
 # get the reaction network
 @timeit to "netgen" prnbng = loadrxnetwork(BNGNetwork(),string(networkname,"bng"), fname); 
 rn = prnbng.rn; u0 = prnbng.u₀; p = prnbng.p; 
-@timeit to "addodes" addodes!(rn; build_jac=false, build_symfuncs=false)
+@timeit to "addodes" addodes!(rn; build_jac=false, build_symfuncs=false, build_paramjac=false)
+#@timeit to "baddodes" addodes!(rn; sparse_jac=true, build_symfuncs=false, build_paramjac=false)
 @timeit to "ODEProb" oprob = ODEProblem(rn, u0, (0.,tf), p)
 u = copy(u0);
 du = similar(u);
 @timeit to "f1" rn.f(du,u,p,0.)
 @timeit to "f2" rn.f(du,u,p,0.)
 show(to)
+println()
 
 # note solvers run faster the second time 
 
@@ -35,5 +37,6 @@ show(to)
 
 # CVODE_BDF with LU 
 reset_timer!(to); @timeit to "CVODE_BDF-1" begin sol = solve(oprob,CVODE_BDF(),dense=false, abstol=1e-8, reltol=1e-8); end; show(to)
-reset_timer!(to); @timeit to "CVODE_BDF-2" begin sol = solve(oprob,CVODE_BDF(),dense=false, abstol=1e-8, reltol=1e-8); end; show(to)
-
+@timeit to "CVODE_BDF-2" begin sol = solve(oprob,CVODE_BDF(),dense=false, abstol=1e-8, reltol=1e-8); end; show(to)
+@timeit to "KenCarp4-1" begin sol = solve(oprob,KenCarp4(autodiff=false),dense=false, calck=false, abstol=1e-8, reltol=1e-8); end; show(to)
+@timeit to "KenCarp4-2" begin sol = solve(oprob,KenCarp4(autodiff=false),dense=false, calck=false, abstol=1e-8, reltol=1e-8); end; show(to)
