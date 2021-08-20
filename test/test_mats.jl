e7b600d1c2a3d9071ed531096cd3d1d8d2849439
Nikhil Yewale <yewale.nikhil65@gmail.com>
2021-08-20 20:39:45 +0530

    adding tests for  LoadReacCompNetwork
    adding tests for LoadReacCompNetwork statements

--- test/test_mats.jl
+++ test/test_mats.jl
@@ -21,12 +21,25 @@ prodstoich = [0 1 0;
               0 0 1;
               1 1 0;
               3 0 0]'
-prn = loadrxnetwork(MatrixNetwork(), pars, substoich, prodstoich; params=pars)
+compstoichmat =[2  0  1  0  0  3;
+                 0  1  1  0  0  0;
+                 0  0  0  1  3  0]
+incidencemat  = [-1   1   0   0   0;
+                 1  -1   0   0   0;
+                 0   0  -1   1   0;
+                 0   0   1  -1   0;
+                 0   0   0   0  -1;
+                 0   0   0   0   1]
+prn = loadrxnetwork(MatrixNetwork(), pars, substoich, prodstoich; params=pars) # dense version
+@test rs == prn.rn
+prn = loadrxnetwork(MatrixNetwork(), pars, sparse(substoich), sparse(prodstoich); params=pars) # sparse version
 @test rs == prn.rn
 
-# sparse version
-prn = loadrxnetwork(MatrixNetwork(), pars, sparse(substoich), sparse(prodstoich); params=pars)
-@test rs == prn.rn
+prn = LoadReacCompNetwork( pars, compstoichmat, incidencemat; params=pars) # dense version
+@test rs == prn
+prn = LoadReacCompNetwork( pars, sparse(compstoichmat), sparse(incidencemat); params=pars) # sparse version
+@test rs == prn
+
 
 # version with hard coded rates (no parameter symbols)
 rs = @reaction_network begin
@@ -36,12 +49,15 @@ rs = @reaction_network begin
     4., S3 --> S1 + S2
     5., 3S3 --> 3S1
 end 
-prn = loadrxnetwork(MatrixNetwork(), convert.(Float64,1:5), substoich, prodstoich)
+prn = loadrxnetwork(MatrixNetwork(), convert.(Float64,1:5), substoich, prodstoich)  # dense version
+@test rs == prn.rn
+prn = loadrxnetwork(MatrixNetwork(), convert.(Float64,1:5), sparse(substoich), sparse(prodstoich))  # sparse version 
 @test rs == prn.rn
 
-# sparse version
-prn = loadrxnetwork(MatrixNetwork(), convert.(Float64,1:5), sparse(substoich), sparse(prodstoich))
-@test rs == prn.rn
+prn = LoadReacCompNetwork(convert.(Float64,1:5), compstoichmat, incidencemat)  # dense version
+@test rs == prn
+prn = LoadReacCompNetwork(convert.(Float64,1:5), sparse(compstoichmat), sparse(incidencemat)) # sparse version
+@test rs == prn
 
 
 
@@ -57,9 +73,14 @@ end k1 k2 k3 k4 k5
 @variables A(t) B(t) C(t)
 species = [A,B,C]
 rates = [k1*A,k2,k3,k4,k5]
-prn = loadrxnetwork(MatrixNetwork(), rates, substoich, prodstoich; species=species, params=pars)
+prn = loadrxnetwork(MatrixNetwork(), rates, substoich, prodstoich; species=species, params=pars)  # dense version
+@test rs == prn.rn
+prn = loadrxnetwork(MatrixNetwork(), rates, sparse(substoich), sparse(prodstoich); species=species, params=pars) # sparse version
 @test rs == prn.rn
 
-# sparse version
-prn = loadrxnetwork(MatrixNetwork(), rates, sparse(substoich), sparse(prodstoich); species=species, params=pars)
-@test rs == prn.rn
+prn = LoadReacCompNetwork(rates, compstoichmat, incidencemat; species = species, params=pars)  # dense version
+@test rs == prn
+prn = LoadReacCompNetwork(rates, sparse(compstoichmat), sparse(incidencemat); species = species, params=pars)  # sparse version
+@test rs == prn
+
+
