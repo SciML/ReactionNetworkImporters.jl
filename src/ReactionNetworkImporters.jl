a9862a424d3b42a35f1c46651663f187fe92f3cc
Torkel <torkel.loman@gmail.com>
2023-12-30 11:13:33 +0100

    move functions

--- src/ReactionNetworkImporters.jl
+++ src/ReactionNetworkImporters.jl
@@ -42,6 +42,24 @@ end
 
 export BNGNetwork, MatrixNetwork, ParsedReactionNetwork, ComplexMatrixNetwork
 
+# Ensures that `prnbng.u₀` works.
+function Base.getproperty(prnbng::ParsedReactionNetwork, name::Symbol)
+    if name === :u₀
+        return getfield(prnbng, :u0)
+    else
+        return getfield(prnbng, name)
+    end
+end
+
+# Ensures that `prnbng.u₀ = ...` works.
+function Base.setproperty!(prnbng::ParsedReactionNetwork, name::Symbol, x)
+    if name === :u₀
+        return setfield!(prnbng, :u0, x)
+    else
+        return setfield!(prnbng, name, x)
+    end
+end
+
 # parsers
 include("parsing_routines_bngnetworkfiles.jl")
 include("parsing_routines_matrixnetworks.jl")
